0f3d5347bd7bc6fb092e6beae9b53e0688839c9e
ArzuBayramova <arzubayramova110@gmail.com>
2023-12-13 12:04:27 +0400

    versiya1

---
+++ js.js
@@ -0,0 +1,14 @@
+let value =5;
+
+console.log(value !== "5");
+console.log(value > 4);
+console.log(value < 25)
+console.log(value !== " ");
+console.log(value !== undefined );
+
+
+let a =prompt("yaşınızı qeyd edin")
+
+if(a<18){console.log("Access denied")};
+if(a>18){console.log("Access granted")};
+
